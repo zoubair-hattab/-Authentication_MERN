999e1d5edd63a411a5c120127cbacbfae0ea06ee
zoubair-hattab <elhattabzoubair93@gmail.com>
2024-02-25 21:08:46 +0100

    create sign up api

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 mongoose
   .connect(process.env.DB_URL)
@@ -14,3 +17,6 @@ const app = express();
 app.listen(3000, () => {
   console.log('server is runing on port 3000');
 });
+app.use(express.json());
+app.use('api/v2/user', userRoutes);
+app.use('api/v2/auth', authRoutes);
