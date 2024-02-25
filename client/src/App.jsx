ffbfc5878454663486826af2aa6ad8067f5b64f3
zoubair-hattab <elhattabzoubair93@gmail.com>
2024-02-25 19:30:16 +0100

    Header pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
+import Header from './component/Header.jsx';
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
