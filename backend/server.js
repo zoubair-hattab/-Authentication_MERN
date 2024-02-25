e0f23571a05830821ee65f436a03b4e703166d12
zoubair-hattab <elhattabzoubair93@gmail.com>
2024-02-25 20:24:55 +0100

    connect to mongoDb

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log('Connect To MongoDB');
+  })
+  .catch((err) => {
+    console.log(`There is an error ${err?.message}`);
+  });
 const app = express();
 app.listen(3000, () => {
   console.log('server is runing on port 3000');
